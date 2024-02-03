45345ecd807304a78c13f76cd427eeb18d56c907
s0k08m1 <saurav.kumar5@walmart.com>
2024-02-03 18:21:39 +0530

    added assests

--- flautist-web-app/src/components/Banner.js
+++ flautist-web-app/src/components/Banner.js
@@ -89,19 +89,7 @@ export const Banner = () => {
               )}
             </TrackVisibility>
           </Col>
-          <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible ? "animate__animated animate__zoomIn" : ""
-                  }
-                >
-                  <img src={headerImg} alt="Header Img" />
-                </div>
-              )}
-            </TrackVisibility>
-          </Col>
+          <Col xs={12} md={6} xl={5}></Col>
         </Row>
       </Container>
     </section>
